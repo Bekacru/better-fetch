7c8b7df90885c467e44f4524d2b522e6c2f3254a
Bereket Engida <bekacru@gmail.com>
2024-07-06 23:37:12 +0300

    fix: remove react exports

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: {
 		index: "./src/index.ts",
-		react: "./src/react.ts",
 		typed: "./src/typed.ts",
 	},
 	splitting: false,
