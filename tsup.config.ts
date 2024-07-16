8c238826c9510b29d18bb3fa055339b178e3b589
Bereket Engida <bekacru@gmail.com>
2024-07-16 23:39:19 +0300

    chore: build config

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,8 @@ export default defineConfig({
 	entry: ["./src/index.ts"],
 	splitting: false,
 	sourcemap: true,
-	format: ["esm"],
+	format: ["esm", "cjs"],
 	dts: true,
 	clean: true,
+	external: ["zod"],
 });
