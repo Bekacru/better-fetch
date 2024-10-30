b15eeabfb9932fcbd1f7ce7b8afe3248fff474d9
LovelessCodes <hello@loveless.codes>
2024-10-30 12:17:33 +0100

    feat: add themes chore(deps): update

--- docs/app/source.ts
+++ docs/app/source.ts
@@ -1,13 +1,8 @@
-import { docs, meta, changelog as _changelog } from "@/.source";
+import { docs, meta } from "@/.source";
 import { createMDXSource } from "fumadocs-mdx";
 import { loader } from "fumadocs-core/source";
 
 export const source = loader({
 	baseUrl: "/docs",
 	source: createMDXSource(docs, meta),
-});
-
-export const changelog = loader({
-	baseUrl: "/changelog",
-	source: createMDXSource(_changelog, meta),
 });
--- docs/tailwind.config.js
+++ docs/tailwind.config.js
@@ -1,4 +1,8 @@
 import { createPreset } from "fumadocs-ui/tailwind-plugin";
+import defaultTheme from "tailwindcss/defaultTheme";
+const {
+	default: flattenColorPalette,
+} = require("tailwindcss/lib/util/flattenColorPalette");
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -11,8 +15,42 @@ export default {
 		"./node_modules/fumadocs-ui/dist/**/*.js",
 	],
 	presets: [createPreset()],
+	plugins: [
+		require("tailwindcss-animate"),
+		addVariablesForColors,
+		function ({ matchUtilities, theme }) {
+			matchUtilities(
+				{
+					"bg-grid": (value) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+						)}")`,
+					}),
+					"bg-grid-small": (value) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`,
+						)}")`,
+					}),
+					"bg-dot": (value) => ({
+						backgroundImage: `url("${svgToDataUri(
+							`<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`,
+						)}")`,
+					}),
+				},
+				{
+					values: flattenColorPalette(theme("backgroundColor")),
+					type: "color",
+				},
+			);
+		},
+	],
 	theme: {
 		extend: {
+			fontFamily: {
+				sans: ["var(--font-geist-sans)"],
+				mono: ["var(--font-geist-mono)"],
+				display: [...defaultTheme.fontFamily.sans],
+			},
 			colors: {
 				border: "hsl(var(--border))",
 				input: "hsl(var(--input))",
@@ -87,3 +125,14 @@ export default {
 		},
 	},
 };
+
+function addVariablesForColors({ addBase, theme }) {
+	let allColors = flattenColorPalette(theme("colors"));
+	let newVars = Object.fromEntries(
+		Object.entries(allColors).map(([key, val]) => [`--${key}`, val]),
+	);
+
+	addBase({
+		":root": newVars,
+	});
+}
