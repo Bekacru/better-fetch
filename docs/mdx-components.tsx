0d4e7068806a690a436d4702b8757fbe3a5800d1
LovelessCodes <hello@loveless.codes>
2024-10-30 12:51:33 +0100

    fix: remove search fix: use fumadocs-twoslash/ui instead of fumadocs/twoslash

--- docs/mdx-components.tsx
+++ docs/mdx-components.tsx
@@ -3,7 +3,7 @@ import { Callout } from "fumadocs-ui/components/callout";
 import { Card, Cards } from "fumadocs-ui/components/card";
 import { Tab, Tabs } from "fumadocs-ui/components/tabs";
 import defaultComponents from "fumadocs-ui/mdx";
-import { Popup, PopupContent, PopupTrigger } from "fumadocs-ui/twoslash/popup";
+import { Popup, PopupContent, PopupTrigger } from "fumadocs-twoslash/ui";
 import type { MDXComponents } from "mdx/types";
 import Link from "next/link";
 
